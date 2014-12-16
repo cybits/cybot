d1ba25792e711d87fa5d51dcee24c197cd2f1818
boreq <boreq@sourcedrops.com>
2014-12-17 00:17:16 +0100

    Cleaned up the code. Removed unnecessary imports. Removed redundant comments. Added port configuration. Moved comments explaining purpose of functions to docstrings. Changed " to ' since we are apprently using that. String formatting is easier to read.

--- irc.py
+++ irc.py
@@ -1,43 +1,46 @@
-# Import some necessary libraries.
 import socket
-import string
-import sched
 import requests
 from commands import get_command
 
-# Some basic variables used to configure the bot
-server = "irc.rizon.net"  # Server
-channel = "#/g/punk"  # Channel
-botnick = "cybits"  # bot's nick
+
+# Basic config
+server = "irc.rizon.net"
+port = 6667
+channel = "#/g/punk"
+botnick = "cybits"
 commandprefix = "."
 
 
-def sendmsg(recipient, msg):  # This is the send message function, it simply sends messages to the channel.
+def sendmsg(recipient, msg):
+    """Sends a message."""
     if msg and isinstance(msg, tuple):
         for i in msg:
-            ircsock.send("PRIVMSG " + recipient + " :" + i + "\n")
+            ircsock.send("PRIVMSG %s :%s\n" % (recipient, i))
     elif msg:
-        ircsock.send("PRIVMSG " + recipient + " :" + msg + "\n")
+        ircsock.send("PRIVMSG %s :%s\n" % (recipient, msg))
 
 
-def joinchan(chan):  # This function is used to join channels.
-    ircsock.send("JOIN " + chan + "\n")
+def joinchan(chan):
+    """Joins a channel."""
+    ircsock.send("JOIN %s\n" % chan)
 
 
 def parsemsg(s):
+    """Breaks a message from an IRC server into its prefix, command, and
+    arguments.
+    """
     # TODO: Refactor the fuck out of this
-    # Breaks a message from an IRC server into its prefix, command, and arguments.
-    prefix = ''
+    prefix = ""
     trailing = []
     retargs = []
     raw = s
     command = ""
     if not s:
         pass
-    if s[0] == ':':
-        prefix, s = s[1:].split(' ', 1)
-    if s.find(' :') != -1:
-        s, trailing = s.split(' :', 1)
+    if s[0] == ":":
+        prefix, s = s[1:].split(" ", 1)
+    if s.find(" :") != -1:
+        s, trailing = s.split(" :", 1)
         args = s.split()
         args.append(trailing)
         commands = args[2][1:].strip().split() if len(args) >= 3 else ""
@@ -83,23 +86,22 @@ def process_data(data):
     lines = data.splitlines()
     # There is at least one newline => this data chunk contains the end of at
     # least one command. If previous command was stored then it is complete now.
-    if '\n' in data and _partial_data:
+    if "\n" in data and _partial_data:
         lines[0] = _partial_data + lines[0]
         _partial_data = None
     # Store partial data.
-    if not data.endswith('\n'):
+    if not data.endswith("\n"):
         if _partial_data is None:
-            _partial_data = ''
+            _partial_data = ""
         _partial_data += lines.pop()
     return lines
 
 
 ircsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-ircsock.connect((server, 6667))  # connect to the server using the port 6667
-ircsock.send("USER " + botnick + " " + botnick + " " + botnick + " :some stuff\n")  # user authentication
-ircsock.send("NICK " + botnick + "\n")  # here we actually assign the nick to the bot
-joinchan(channel)  # Join the channel using the functions we previously defined
-
+ircsock.connect((server, port))
+ircsock.send("USER %s %s %s :some stuff\n" % (botnick, botnick, botnick))
+ircsock.send("NICK %s\n" % botnick)
+joinchan(channel)
 
 while True:
     data = ircsock.recv(1024)
