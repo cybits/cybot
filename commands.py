5660498f2fbedce45db6089bcfa0ce4a38e8da91
lich <lich@airmail.cc>
2018-04-10 00:44:59 +0100

    Fixed now playing command in situation of user not playing track now

--- commands.py
+++ commands.py
@@ -992,9 +992,10 @@ def librefm(args):
         libreuser = args["args"][0]
     xml = BeautifulSoup(requests.get("https://libre.fm/2.0/?method=user.getrecenttracks&user={}&page=1&limit=1".format(libreuser)).text, "html.parser")
     try:
-        if xml.lfm.recenttracks.track['nowplaying']:
-            return "User {} is now playing \"{}\" by {} on [{}].".format(libreuser,xml.lfm.recenttracks.find('name').string,xml.lfm.recenttracks.artist.string,xml.lfm.recenttracks.album.string)
-        else:
-            return "User {} last played \"{}\" by {} on [{}].".format(libreuser,xml.lfm.recenttracks.find('name').string,xml.lfm.recenttracks.artist.string,xml.lfm.recenttracks.album.string)
+        try:
+            if xml.lfm.recenttracks.track['nowplaying']:
+                return "User {} is now playing \"{}\" by {} on [{}].".format(libreuser,xml.lfm.recenttracks.find('name').string,xml.lfm.recenttracks.artist.string,xml.lfm.recenttracks.album.string)
+        except KeyError:
+                return "User {} last played \"{}\" by {} on [{}].".format(libreuser,xml.lfm.recenttracks.find('name').string,xml.lfm.recenttracks.artist.string,xml.lfm.recenttracks.album.string)
     except:
         return "User {} does not exist on libre.fm.".format(libreuser)
